2e7975256058283de6c812328cfa45a58557b97a
taehyun517 <taehyun517@naver.com>
2024-03-19 18:27:14 +0900

    테스트

--- app/echo/EchoAssignment.cpp
+++ app/echo/EchoAssignment.cpp
@@ -21,23 +21,64 @@
 // See below for their usage.
 // https://github.com/ANLAB-KAIST/KENSv3/wiki/Misc:-External-Resources#linux-manuals
 
-int EchoAssignment::serverMain(const char *bind_ip, int port,
-                               const char *server_hello) {
+int EchoAssignment::serverMain(const char *bind_ip, int port,const char *server_hello) {
   // Your server code
   // !IMPORTANT: do not use global variables and do not define/use functions
   // !IMPORTANT: for all system calls, when an error happens, your program must
   // return. e.g., if an read() call return -1, return -1 for serverMain.
+  struct sockaddr_in serv_addr = {};
+  struct sockaddr_in client_addr = {};
+  const struct sockaddr clent_addr1 = (struct sockaddr) client_addr;
+  socklen_t client_addr_len = (socklen_t)sizeof(client_addr);
+  char buff[256];
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(inet_addr(bind_ip));
+  serv_addr.sin_port = htons(port);
+  int serv_sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+  if(serv_sock == -1) return -1;
+  if(bind(serv_sock,&serv_addr,sizeof(serv_addr))==-1) return -1;
+  if(listen(serv_sock,3)==-1) return -1;
+  int client_sock = accept(serv_sock, &client_addr1, client_addr_len);
+  if(client_sock == -1) return -1;
 
+  while(1) {
+    memset(buff,0,256);
+    int rd = read(client_sock, buff, sizeof(buff));
+    if(rd == -1) break;
+    submitAnswer(inet_ntop());
+    int wr = write(client_sock, buff, sizeof(buff));
+    if(wr == -1) break;
+  }
+  close(serv_sock);
   return 0;
 }
 
-int EchoAssignment::clientMain(const char *server_ip, int port,
-                               const char *command) {
+int EchoAssignment::clientMain(const char *server_ip, int port,const char *command) {
   // Your client code
   // !IMPORTANT: do not use global variables and do not define/use functions
   // !IMPORTANT: for all system calls, when an error happens, your program must
   // return. e.g., if an read() call return -1, return -1 for clientMain.
+  struct sockaddr_in serv_addr = {};
+  char buff[256];
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr(server_ip);
+  serv_addr.sin_port = htons(port);
+  int client_sock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+  if(client_sock == -1) return -1;
+  if(connect(client_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) return -1;
+  while(1) {
+    memset(buff,0,256);
+    scanf("%255[^\n]", buff);
+    int wr = write(client_sock, buff, strlen(buff));
+    if(wr == -1) break;
+    memset(buff,0,256);
+    int rd = read(client_sock, buff, sizeof(buff));
+    if(rd == -1) break;
 
+  }
+  close(client_sock);
   return 0;
 }
 
